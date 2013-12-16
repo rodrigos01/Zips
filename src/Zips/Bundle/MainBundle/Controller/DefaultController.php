2e0a131b69dd9ed19f963083de8ff51d81223bee
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2013-12-16 14:53:17 -0200

    Inicio dos Zips

--- src/Zips/Bundle/MainBundle/Controller/DefaultController.php
+++ src/Zips/Bundle/MainBundle/Controller/DefaultController.php
@@ -4,36 +4,46 @@ namespace Zips\Bundle\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle"=>"Início", "zipList"=>array()));
+class DefaultController extends Controller {
+  
+  private function getZipList($sql) {
+    $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
+    if($stmt->execute()) {
+      if($stmt->columnCount() > 0) {
+        return $stmt->fetchAll();
+      }
+    } else {
+      throw new DBALException($stmt->errorInfo(), $stmt->errorCode());
     }
+  }
+
+  public function indexAction() {
     
-    public function abertosAction()
-    {
-        return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle"=>"Abertos", "zipList"=>array()));
-    }
+    $sql = "select pa1.dataUpado data, p.id id, p.nome nome, s.nome site, aa.nome atual, pa.nome proximo, p.finalizado, p.revisaoSolicitada, pa1.aprovado  from ((Pacotes p inner join Sites s on p.idSite=s.id) inner join (Pacotes_Ambientes pa1 inner join Ambientes aa on pa1.idAmbiente=aa.id) on p.id=pa1.idPacote) inner join (Pacotes_Ambientes pa2 inner join Ambientes pa on pa2.idAmbiente=pa.id) on p.id=pa2.idPacote where p.finalizado=0 and pa1.upado=1 and (pa1.dataUpado=(select max(dataUpado) from Pacotes_Ambientes where idPacote=p.id)) and pa2.aprovado=0 order by pa1.dataUpado desc";
     
-    public function finalizadosAction()
-    {
-        return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle"=>"Finalizados", "zipList"=>array()));
-    }
+    return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle" => "Início", "zipList" => $this->getZipList($sql)));
+  }
+
+  public function abertosAction() {
+    return $this->indexAction();
+  }
+
+  public function finalizadosAction() {
+    $sql = "select pa1.dataUpado data, p.id id, p.nome nome, s.nome site, aa.nome atual, pa.nome proximo, p.finalizado, p.revisaoSolicitada, pa1.aprovado  from ((Pacotes p inner join Sites s on p.idSite=s.id) inner join (Pacotes_Ambientes pa1 inner join Ambientes aa on pa1.idAmbiente=aa.id) on p.id=pa1.idPacote) inner join (Pacotes_Ambientes pa2 inner join Ambientes pa on pa2.idAmbiente=pa.id) on p.id=pa2.idPacote where p.finalizado=1 and pa1.upado=1 and (pa1.dataUpado=(select max(dataUpado) from Pacotes_Ambientes where idPacote=p.id)) and (pa2.aprovado=0 or pa2.upado=0 or pa2.idAmbiente=pa1.idAmbiente) group by p.id order by pa1.dataUpado desc";
     
-    public function meusZipsAction()
-    {
-        return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle"=>"Meus Zips", "zipList"=>array()));
-    }
+    return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle" => "Finalizados", "zipList" => $this->getZipList($sql)));
+  }
+
+  public function meusZipsAction() {
+    return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle" => "Meus Zips", "zipList" => array()));
+  }
+
+  public function configuracoesAction() {
+    return $this->render('ZipsMainBundle:Default:config.html.twig');
+  }
+
+  public function logoutAction() {
     
-    public function configuracoesAction()
-    {
-        return $this->render('ZipsMainBundle:Default:config.html.twig');
-    }
-    
-    public function logoutAction()
-    {
-        
-    }
-    
+  }
+
 }
