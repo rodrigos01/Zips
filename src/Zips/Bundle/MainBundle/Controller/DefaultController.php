d6d78000f0e9113c124461fb8a8bf0e078b11cf3
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2013-12-16 11:06:02 -0200

    Alterações de layout; base das páginas

--- src/Zips/Bundle/MainBundle/Controller/DefaultController.php
+++ src/Zips/Bundle/MainBundle/Controller/DefaultController.php
@@ -8,6 +8,32 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ZipsMainBundle:Default:index.html.twig');
+        return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle"=>"Início", "zipList"=>array()));
     }
+    
+    public function abertosAction()
+    {
+        return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle"=>"Abertos", "zipList"=>array()));
+    }
+    
+    public function finalizadosAction()
+    {
+        return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle"=>"Finalizados", "zipList"=>array()));
+    }
+    
+    public function meusZipsAction()
+    {
+        return $this->render('ZipsMainBundle:Default:index.html.twig', array("pageTitle"=>"Meus Zips", "zipList"=>array()));
+    }
+    
+    public function configuracoesAction()
+    {
+        return $this->render('ZipsMainBundle:Default:config.html.twig');
+    }
+    
+    public function logoutAction()
+    {
+        
+    }
+    
 }
